7ef1c1294847f43fe3e5516b32b2c8b374c49f22
Ben White <b.white@rocburn.com>
2025-05-23 15:55:04 +0100

    Allow IProductFeedGeneratroService implementations to define the format they generate.  Use this format for output with controller.

---
+++ src/Umbraco.Commerce.ProductFeeds.Core/Features/FeedGenerators/Application/FeedFormat.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Umbraco.Commerce.ProductFeeds.Core.Features.FeedGenerators.Application
+{
+    public enum FeedFormat
+    {
+        Xml,
+        Json
+    }
+}
